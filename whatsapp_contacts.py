845029fed67e2bd4dddca4c290051f17ed77cd01
VINICIUS ALMEIDA DE SOUZA <v.almsou@uol.com.br>
2020-08-23 12:54:20 -0300

    Sleep and Comments
    Change some Things and sen new ideas to UX

--- whatsapp_contacts.py
+++ whatsapp_contacts.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import time
+from time import sleer #better response
 import sys
 
 # Replace below path with the absolute path
@@ -16,7 +16,7 @@ wait = WebDriverWait(driver, 600)
 
 group_title = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.RLfQR")))
 
-time.sleep(5)
+sleep(5)
 for person in driver.find_elements_by_class_name('_2wP_Y'):
     title = person.find_element_by_xpath('div/div/div[2]/div[1]/div[1]/span').text
     #company = person.find_element_by_xpath('.//div[@class="company"]/a').text
--- whatsapp_search_contacts_send_message.py
+++ whatsapp_search_contacts_send_message.py
@@ -3,14 +3,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import time
+from time import sleep #better response
 import sys
 
 # Replace below path with the absolute path
 # to chromedriver in your computer
 
-friends_lists = ['Manikandan PM','Akshay wd']
-
+friends_lists = ['Manikandan PM','Akshay wd'] 
+'''Instead of above we can aprimorate this by import those:
+import openpyxl #To read an Excel(.xlsx) file with contacts names
+import docx  #To read a Word(.docx) file with the message
+import PySimpleGUI #To create a user interface and use NSIS to make ".exe" 
+pls check my project Wpp-Sender @valmsou
+'''
 driver = webdriver.Chrome('../chromedriver')
 
 driver.get("https://web.whatsapp.com/")
@@ -23,7 +28,7 @@ for friend in friends_lists:
 	search.clear()
 	search.send_keys(friend)
 	wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button._3Burg")))
-	time.sleep(3)
+	sleep(3)
 	persons = driver.find_elements_by_class_name('_2wP_Y')
 	print(len(persons))
 	for person in persons:
@@ -33,13 +38,14 @@ for friend in friends_lists:
 				print(person_title.get_attribute("title"))
 				person_contact = person.find_element_by_class_name('_2EXPL')
 				person_contact.click()
+				sleep(2)
 				message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
 				message.send_keys("This is Testing Message from Selenium")
 
 				sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
 				sendbutton.click()
 		except:
-			print("*")
+			print('Send sucessful to: {}',format(person)) 
 			continue
 
 driver.close()
